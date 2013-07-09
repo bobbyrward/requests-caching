c572099a0f454da32fa93cb68cc9bf032a68733c
Bobby R. Ward <bobbyrward@gmail.com>
2013-07-09 00:53:45 -0400

    Add filter usage to adapter

--- requests_caching/adapter.py
+++ requests_caching/adapter.py
@@ -1,6 +1,36 @@
 from requests.adapters import HTTPAdapter
 
+from requests_caching.cache import Cache
+from requests_caching.filters import rfc2616
+
 
 class CacheAdapter(HTTPAdapter):
-    def __init__(self):
+    def __init__(self, cache=None, validation_filter=None, cacheability_filter=None):
         super(CacheAdapter, self).__init__()
+
+        if cache is None:
+            cache = Cache()
+
+        self.cache = cache
+
+        if validation_filter is None:
+            validation_filter = rfc2616.ValidationFilter(cache=self.cache)
+
+        self.validation_filter = validation_filter
+
+        if cacheability_filter is None:
+            cacheability_filter = rfc2616.CacheabilityFilter(cache=self.cache)
+
+        self.cacheability_filter = cacheability_filter
+
+    def send(self, request, **kwargs):
+        return super(CacheAdapter, self).send(request, **kwargs)
+
+    def build_response(self, request, response):
+        return super(CacheAdapter, self).build_response(request, response)
+
+    def can_read_cache(self, request):
+        return self.validation_filter.filter(request)
+
+    def can_cache_response(self, request, response):
+        return self.cacheability_filter.filter(request, response)
--- requests_caching/tests/test_adapter.py
+++ requests_caching/tests/test_adapter.py
@@ -1,8 +1,66 @@
 import unittest
 
+import mock
+
 from requests_caching import adapter
+from requests_caching.cache import Cache
+from requests_caching.filters import rfc2616
 
 
 class TestAdapter(unittest.TestCase):
     def test_creation(self):
-        cache = adapter.CacheAdapter()  #flake8: noqa
+        cache = adapter.CacheAdapter()
+        self.assertIsInstance(cache.cache, Cache)
+        self.assertIsInstance(cache.validation_filter, rfc2616.ValidationFilter)
+        self.assertIsInstance(cache.cacheability_filter, rfc2616.CacheabilityFilter)
+
+    def test_creation_provide_cache(self):
+        mock_cache = mock.MagicMock()
+        cache = adapter.CacheAdapter(cache=mock_cache)
+        self.assertIs(cache.cache, mock_cache)
+
+    def test_creation_provide_validation_filter(self):
+        mock_filter = mock.MagicMock()
+        cache = adapter.CacheAdapter(validation_filter=mock_filter)
+        self.assertIs(cache.validation_filter, mock_filter)
+
+    def test_creation_provide_cacheability_filter(self):
+        mock_filter = mock.MagicMock()
+        cache = adapter.CacheAdapter(cacheability_filter=mock_filter)
+        self.assertIs(cache.cacheability_filter, mock_filter)
+
+    def test_send_calls_base(self):
+        request = mock.MagicMock()
+        cache = adapter.CacheAdapter()
+
+        with mock.patch('requests_caching.adapter.HTTPAdapter.send') as mocked_send:
+            returned = cache.send(request)
+
+        mocked_send.assert_called_once_with(request)
+        self.assertEqual(returned, mocked_send.return_value)
+
+    def test_build_response_calls_base(self):
+        request = mock.MagicMock()
+        response = mock.MagicMock()
+        cache = adapter.CacheAdapter()
+
+        with mock.patch('requests_caching.adapter.HTTPAdapter.build_response') as mocked_br:
+            returned = cache.build_response(request, response)
+
+        mocked_br.assert_called_once_with(request, response)
+        self.assertEqual(returned, mocked_br.return_value)
+
+    def test_can_read_cache(self):
+        validation = mock.MagicMock()
+        request = mock.MagicMock()
+        cache = adapter.CacheAdapter(validation_filter=validation)
+        self.assertIs(cache.can_read_cache(request), validation.filter.return_value)
+        validation.filter.assert_called_once_with(request)
+
+    def test_can_cache_response(self):
+        cacheability = mock.MagicMock()
+        request = mock.MagicMock()
+        response = mock.MagicMock()
+        cache = adapter.CacheAdapter(cacheability_filter=cacheability)
+        self.assertIs(cache.can_cache_response(request, response), cacheability.filter.return_value)
+        cacheability.filter.assert_called_once_with(request, response)
